80aff1d27440260b5976556b80d50121f8327cba
EmirD <dedic.emir17@gmail.com>
2023-07-20 12:29:21 +0200

    Added the missing components
    TO DO:
    Fill the empty components with Content as shown in the task example
    
    DONE:
    Write new components for the project

--- pillows/src/App.js
+++ pillows/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Homenavbar from "./components/Homenavbar/Homenavbar";
+import ContentPage from "./components/ContentPage/ContentPage";
+import Footerbar from "./components/Footerbar/Footerbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Homenavbar />
+      <ContentPage />
+<Footerbar />
+    </>
+  );
 }
 
 export default App;
